40652f53dc13611e69b56c1a308757f49482896d
Jason Riordan <jriordan001@gmail.com>
2016-08-15 23:23:10 -0400

    Z00A: PEMDAS, we need device flags before we build CM things
    Change-Id: Ib7a3f9661b43f7bfc4c3dbb4fcb55e3b8b5efb96
    
    Conflicts:
    	to.mk

--- to.mk
+++ to.mk
@@ -5,10 +5,10 @@ TARGET_SCREEN_WIDTH := 1080
 # Inherit some common OctOS stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/to/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/asus/Z00A/device.mk)
+
+$(call inherit-product, vendor/to/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/to/config/nfc_enhanced.mk)
